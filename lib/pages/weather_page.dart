07de3f6963bd289c01586a32e56208dcbdaa9630
amtech1 <139146067+amtech1@users.noreply.github.com>
2023-11-06 17:20:39 +0500

    complete weather app

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weatherapp/services/weather_service.dart';
 
-class WeatherPage extends StatelessWidget {
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(  );
+  State<WeatherPage> createState()=>_WeatherPageState();
+}
+class _WeatherPageState extends State<WeatherPage>{
+  
+
+  final _WeatherService=WeatherService('f70b321918ba254c9294bec8e40b4d05');
+  Weather?_Weather;
+  _fetchWeather()async{
+
+    String cityName=await _WeatherService.getCurrentCity();
+    try{
+      final weather= await _WeatherService.getWeather(cityName);
+      setState(() {
+        _Weather=weather;
+      });
+    }
+    catch(e){
+      print(e);
+    }
   }
+
+  String getWeatherAnimation(String? mainCondition){
+if(mainCondition==null) return 'assets/sun.json';
+switch (mainCondition.toLowerCase()) {
+  case 'rain':
+  return'assets/rain.json';
+      case 'cloud':
+  return'assets/cloud.json';
+    
+    break;
+    default:
+      return'assets/cloud.json';
+
+}
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
+  @override
+  
+    Widget build(BuildContext context) {
+return Scaffold(
+body: Center(
+  child: Column(
+    
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+
+      Text(_Weather?.cityName ??'loading city..'),
+      Lottie.asset(getWeatherAnimation(_Weather?.mainCondition)),
+      Text('${_Weather?.temperature.round()}C'),
+         Text(_Weather?.mainCondition ??'  '),
+    ],
+  ),
+),);
+  }
+
 }
